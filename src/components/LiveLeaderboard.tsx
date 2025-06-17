13d658f4fd50b62bea125b4a0ebe90a1e7dc93cd
Balogun Abdulsamad <abdulsamadbalogun25@gmail.com>
2025-06-17 20:38:14 +0100

    Update CTA buttons links

--- src/components/LiveLeaderboard.tsx
+++ src/components/LiveLeaderboard.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { Router } from 'next/router';
 import { useRouter } from 'next/navigation';
 
 interface LeaderboardEntry {
