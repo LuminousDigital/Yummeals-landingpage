ea84879744e892e0f0f23faf93060ea02e8141d9
Balogun Abdulsamad <abdulsamadbalogun25@gmail.com>
2025-06-17 17:29:16 +0100

    Exclude .next directory from version control

--- src/components/PromoSection.tsx
+++ src/components/PromoSection.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const PromoSection = () => {
   return (
     <section
-      className="flex flex-col items-center justify-center  px-4 py-15 md:py-0 text-center bg-cover bg-center bg-no-repeat  "
+      className="flex flex-col items-center justify-center px-4 text-center bg-center bg-no-repeat bg-cover py-15 md:py-0 "
       style={{ backgroundImage: "url(/soon.png)" }}
     >
-      <div className=" bg-opacity-50 rounded-lg w-full mt-4 ">
+      <div className="w-full mt-4 bg-opacity-50 rounded-lg ">
         {/* <Image src="/lauch.png" alt="Yummeals" width={400} height={400} /> */}
 
         <div className="relative w-full aspect-square mx-auto flex items-center justify-between h-[40vh] sm:h-[90vh] sm:pb-16 sm:pt-6">
           <div className="lg:w-[40%] xl:[100%]  sm:w-[70%] md:w-[70%] w-full mx-auto relative">
-            {/* <div className="max-w-5xl mx-auto p-4 md:p-6 relative"> */}
+            {/* <div className="relative max-w-5xl p-4 mx-auto md:p-6"> */}
             {/* Main heading */}
             <div className="absolute inset-0 top-[5%] mobile375:top-[2%] xs:top-[5%] sm:top-[3%]  md:top-[3%] lg:top-[3%] text-center font-extrabold text-3xl xs:text-[14px] sm:text-4xl md:text-4xl lg:text-4xl  leading-[1.6] xs:leading-[1.1] sm:leading-[1.15] lg:leading-[1.8] md:leading-[190%] xl:leading-[190%] xl:text-4xl  xs:tracking-normal sm:tracking-[-0.01em] lg:tracking-[-0.02em] px-0.5 xs:px-1 sm:px-2 md:px-0 pt-1 xs:pt-2 sm:pt-0 tracking-[1px] sm:tracking-0  z-50">
             Get <span className="text-[#F25B0A] font-extrabold xl:text-6xl  text-3xl sm:text-4xl md:text-5xl  ">FREE FOOD </span> for a
@@ -21,13 +21,13 @@ const PromoSection = () => {
            
 
             {/* Circle with food images */}
-            {/* <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-3xl mx-auto aspect-square"> */}
-            <div className="relative   mx-auto aspect-square">
+            {/* <div className="relative w-full max-w-xs mx-auto sm:max-w-sm md:max-w-2xl lg:max-w-3xl aspect-square"> */}
+            <div className="relative mx-auto aspect-square">
               {/* Center orange circle with text */}
-              <div className="absolute inset-0 flex items-center justify-center z-10">
+              <div className="absolute inset-0 z-10 flex items-center justify-center">
                 <div className="bg-white  text-black rounded-full lg:w-[60%] lg:h-[60%]  w-[55%] h-[55%] p-4 flex flex-col items-center justify-center  xs:p-2 sm:p-4 md:p-6 lg:p-4 xl:p-2 ">
-                  <p className="w-full text-center text-black font-poppins font-extrabold sm:text-[2rem] xl:text-2xl lg:text-[1.0rem] lg:leading-[150%] xl:leading-[180%]  leading-[150%] text-xs sm:mt-8 md:text-[1.2rem] sm:text-2xl md:mt-3  xl:mt-2  p-2 xl:p-2">
-                  Promo runs from June  <br /> 7th - July 6th, 2025
+                  <p className="w-full text-center text-black font-poppins font-semibold sm:text-[2rem] xl:text-2xl lg:text-[1.0rem] lg:leading-[150%] xl:leading-[180%]  leading-[150%] text-sm  sm:mt-8 md:text-[1.2rem] sm:text-2xl md:mt-3  xl:mt-2  p-2 xl:p-2">
+                  Promo runs from   <br /> <span className="font-extrabold">June 7th - July 6th, 2025</span>
                   </p>
                   <p className="w-full text-center font-poppins font-medium sm:text-[1.5rem] lg:text-[o.5rem] text-sm md:text-[1rem] xl:text-2xl xl:leading-[100%]">
             
@@ -67,7 +67,7 @@ const PromoSection = () => {
               </div> */}
 
               <div className="absolute z-20 top-[50%] right-[10%] xl:right-[2%] sm:right-[8%] transform translate-x-1/9 -translate-y-1/2">
-                {/* <div className="absolute z-20 top-1/3 right-0 transform translate-x-1/9 -translate-y-1/2"> */}
+                {/* <div className="absolute right-0 z-20 transform -translate-y-1/2 top-1/3 translate-x-1/9"> */}
                 <div className="relative w-20 h-20 sm:w-25 sm:h-25 md:w-30 md:h-30 lg:w-25 lg:h-25 xl:w-37 xl:h-37">
                   <Image
                     src="/vegetable-soup.png"
@@ -78,7 +78,7 @@ const PromoSection = () => {
                 </div>
               </div>
 
-              <div className="absolute z-20 bottom-1/5 right-1/4 transform translate-x-1/2 translate-y-1/4 xl:right-1/5">
+              <div className="absolute z-20 transform translate-x-1/2 bottom-1/5 right-1/4 translate-y-1/4 xl:right-1/5">
                 <div className="relative w-20 h-20 sm:w-25 sm:h-25 md:w-30 md:h-30 lg:w-25 lg:h-25 xl:w-37 xl:h-37">
                   <Image
                     src="/uzbek.png"
@@ -90,7 +90,7 @@ const PromoSection = () => {
               </div>
 
               <div className="absolute z-20 bottom-[6%] left-1/2 transform sm:bottom-[6%]  md:bottom-[7%]  xl:bottom-[2%]  lg:bottom-[4%] -translate-x-1/2 translate-y-1/4">
-                {/* <div className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/4"> */}
+                {/* <div className="absolute bottom-0 z-20 transform -translate-x-1/2 left-1/2 translate-y-1/4"> */}
                 <div className="relative w-20 h-20 sm:w-25 sm:h-25 md:w-30 md:h-30 lg:w-25 lg:h-25 xl:w-37 xl:h-37">
                   <Image
                     src="/fried-rice.png"
@@ -102,7 +102,7 @@ const PromoSection = () => {
               </div>
 
               <div className="absolute z-20 lg:top-[50%]  lg:right-[0%] xl:left-[2%] top-[50%]  left-[7%] transform -translate-x-1/16  -translate-y-1/2  ">
-                {/* <div className="absolute z-20 top-20  left-0 transform -translate-x-1/16 lg:-translate-y-1/8 -translate-y-1/2  "> */}
+                {/* <div className="absolute left-0 z-20 transform -translate-y-1/2 top-20 -translate-x-1/16 lg:-translate-y-1/8 "> */}
                 <div className="relative w-20 h-20 sm:w-25 sm:h-25 md:w-30 md:h-30 lg:w-25 lg:h-25 xl:w-37 xl:h-37">
                   <Image
                     src="/burger.png"
@@ -114,7 +114,7 @@ const PromoSection = () => {
               </div>
 
               <div className="absolute z-20 bottom-1/5 left-[22%] lg:left-[25%] xl:left-[20%] xl:bottom-1/6 transform -translate-x-1/2 translate-y-1/5">
-                {/* <div className="absolute z-20 bottom-1/4 left-1/6 transform -translate-x-1/2 translate-y-1/5"> */}
+                {/* <div className="absolute z-20 transform -translate-x-1/2 bottom-1/4 left-1/6 translate-y-1/5"> */}
                 <div className="relative w-20 h-20 sm:w-25 sm:h-25 md:w-30 md:h-30 lg:w-25 lg:h-25 xl:w-37 xl:h-37">
                   <Image
                     src="/Amala.png"
